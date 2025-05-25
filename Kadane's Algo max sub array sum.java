2142ad6a1f3fd2854a864950af98a21e4556f5a5
shaikhafeezsujan <83467773+shaikhafeezsujan@users.noreply.github.com>
2025-05-25 21:32:57 +0530

    Add files via upload

---
+++ Kadane's Algo max sub array sum.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Kadanes_subarray {
+    public static void maxSubArray(int[] arr){
+        int curr =0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            curr+=arr[i];
+            if(curr<0){
+                curr=0;
+            }
+            max=Math.max(curr,max);
+        }
+        
+        System.out.println(max);
+    }
+    public static void main(String[] args){
+        int [] arr = {-2,-3,4,-1,-2,1,5,-3};
+        maxSubArray(arr);
+    }
+}
